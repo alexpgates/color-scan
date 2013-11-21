61134fc82f71d28f854d8839d8f9f6225f2e0360
Alex P. Gates <alex@whatcheer.com>
2013-11-20 23:49:17 -0600

    better settings

--- settings.php
+++ settings.php
@@ -1,8 +1,9 @@
 <?php 
-$num_lights = 3;
-$num_colors = 3;
-$path = "/path/to/whatever/directory/"; // path to where your scanned images are stored
+$color_mode = 'multi'; // "multi" or "single" - multi uses a different color for each light, single uses the most dominant color for a group defined in $group_id
+$light_ids = array( 1, 2, 3 ); // ids of your Hue lights that you want to use with color-scan
+$group_id = 0; // set this if using single color mode;
+$path = "/path/to/your/images/";
 $bridge = 'x.x.x.x'; // ip address of your internal bridge
-$hue_key = 'your-hue-dev-user'; // valid api user
-$debug = 'true'; // uncomment this for some debug info
+$hue_key = 'hue-user'; // valid api user
+//$debug = 'debug'; // uncomment this for some debug info
 ?>
